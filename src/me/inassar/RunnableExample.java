b3d6f46bcfec5c79d50c6fa544a5a547b21a490a
Ahmed Nassar <ahmed.nassar163@gmail.com>
2019-04-23 13:07:22 +0200

    Runnable using lambda

---
+++ src/me/inassar/RunnableExample.java
@@ -0,0 +1,19 @@
+package me.inassar;
+
+public class RunnableExample {
+
+    public static void main(String[] args) {
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Printed Inside Runnable");
+            }
+        });
+        thread.run();
+
+        Thread lambdaThread = new Thread(() -> System.out.println("Printed Inside Lambda Runnable"));
+        lambdaThread.run();
+    }
+
+}
